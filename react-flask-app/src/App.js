d7a074cbc03ba81738cac9f9bc64ed688b5ca75b
Dillon Durrant <djdillondurrant@gmail.com>
2022-09-14 21:39:10 -0600

    filler content

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
+//import logo from 'assets/images/cctransparent-01.png';
+import cclogo from './assets/images/cctransparent-01.png';
 import './App.css';
 
 function App() {
@@ -14,9 +15,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <div className="navbar">
+        <img src={cclogo} className="cc-logo" alt="logo" />
+          <a href="#">Home</a>
+          <a href="#">Find Your Castle</a>
+          <a href="#">About Us</a>
+          <a href="#">Contact Us</a>
+        </div>
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload. Or else.
         </p>
         <a
           className="App-link"
@@ -25,9 +32,18 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
         <p>Message says: {helloMessage?.message}.</p>
       </header>
+      <div className="overview">
+        <h1>Let's Start-y the Party!</h1>
+        <p>Here at Castle Cloud, we don't just rent castles - we create memories. 
+          Each castle is checked for safety and cleaned thoroughly before each rental to keep your party going!</p>
+        <a href="#">Start browsing now!</a>
+      </div>
+      <div className="footer">
+        <h1>Footer goes here</h1>
+      </div>
     </div>
   );
 }
