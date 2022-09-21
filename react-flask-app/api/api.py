bc2340b9180a0e8ece8635d1bf89fb8dc622316f
Dillon Durrant <djdillondurrant@gmail.com>
2022-09-21 14:26:57 -0600

    GET request works, about to break POST

--- react-flask-app/api/api.py
+++ react-flask-app/api/api.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route('/message')
 def hello_world():
     return ({'Message': 'Hello World'})
+
+@app.route('/testMessage')
+def TestMessage(testMessage):
+    return testMessage
+
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    # postData = request.get_json()
+    # newPost = PostTest(content=postData['content'])
+    if request.method == 'POST':
+        #user = request.form['testApi']
+        return f"Posted!"
+    else:
+        return ({'GetTest' : "Got a GET request!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -5,38 +5,66 @@ import './App.css';
 
 function App() {
   const[helloMessage, setMessage] = useState({message: 'friend'});
-
+  const [ApiTest, setApiTest] = useState('default');
+ 
   useEffect(() => {
     fetch('/message').then(res => res.json()).then(data => {
       setMessage({message: data.Message});
     });
   }, []);
 
+  // useEffect((ApiTest) => {
+  //   fetch('/testMessage').then(res => res.json()).then(data => {
+  //     setApiTest({apiTest: data.apiTest});
+  //   });
+  // }, [ApiTest]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({'phrase': ApiTest})
+    };
+    console.log(fetch('/login', requestOptions));
+    return null;
+    // alert(`The message you entered was: ${ApiTest}`)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="navbar">
-        <img src={cclogo} className="cc-logo" alt="logo" />
+          {/* <img src={cclogo} className="cc-logo" alt="logo" /> */}
           <a href="#">Home</a>
           <a href="#">Find Your Castle</a>
           <a href="#">About Us</a>
           <a href="#">Contact Us</a>
         </div>
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload. Or else.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
         <p>Message says: {helloMessage?.message}.</p>
+        <p>Sent Message says: {ApiTest}</p>
       </header>
       <div className="overview">
         <h1>Let's Start-y the Party!</h1>
+
+        <button onClick={getRequest}>Test Get API</button>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="sampleString">Test phrase:
+            <input 
+              type="text" 
+              value={ApiTest}
+              name="testApi"
+              onChange={(e) => setApiTest(e.target.value)}
+            />
+          </label>
+          <input 
+            type="submit"
+            value="Post Test"
+          />
+        </form>
+
+        <h2>Cool inflating graphic here?</h2>
         <p>Here at Castle Cloud, we don't just rent castles - we create memories. 
           Each castle is checked for safety and cleaned thoroughly before each rental to keep your party going!</p>
         <a href="#">Start browsing now!</a>
@@ -46,6 +74,18 @@ function App() {
       </div>
     </div>
   );
+}
+
+export function getRequest(){
+  const requestOptions = {
+    method: 'GET',
+    // headers: { 'Content-Type': 'application/json' },
+    // body: JSON.stringify({phrase: 'Hello API'})
+  };
+  console.log(fetch('/login', requestOptions)
+  .then(response => response.json())
+  );
+  return null;
 }
 
 export default App;
